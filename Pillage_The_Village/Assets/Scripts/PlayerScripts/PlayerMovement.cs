b76aa43eec69ca61a03057dbf7e59afb6fe778c8
Will M <willmcb234@gmail.com>
2020-03-19 10:02:11 -0400

    AddedPlayermovement

--- Pillage_The_Village/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Pillage_The_Village/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Player values")]
+    public float runSpeed;
+    public float jumpForce;
+    public int jumpCount = 0;
     [Header("Bools")]
     public bool isPaused;
+    public bool canJump;
+    public bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +22,45 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float currentYVel = GetComponent<Rigidbody2D>().velocity.y;
+        if(jumpCount == 0 || jumpCount == 1)
+        {
+            canJump = true;
+            isGrounded = true;
+        }
+        else if(jumpCount > 1)
+        {
+            canJump = false;
+            isGrounded = false;
+        }
  ////////Player inputs and movements/////////
         if (Input.GetKeyDown(KeyCode.P))
         {
             isPaused = true;
         }//Sets game to pause
-        
+      if(isPaused == false)
+        {
+            if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(runSpeed, currentYVel);
+                transform.localScale = new Vector2(1, transform.localScale.y);
+            }//moving right
+            if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(-runSpeed, currentYVel);
+                transform.localScale = new Vector2(-1, transform.localScale.y);
+            }//moving left
+            if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+            {
+                if(canJump == true && isGrounded == true)
+                {
+                    GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+                }
+            }//jumping
+        }// stuff that is allowed to happen if the game is not paused
+    }
+    public void unPauseGame()
+    {
+        isPaused = false;
     }
 }
