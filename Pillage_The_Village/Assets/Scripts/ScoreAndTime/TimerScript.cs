677715530a68abf6531e48c0544e29902e95855b
Will M <willmcb234@gmail.com>
2020-04-02 12:38:22 -0400

    Attack

--- Pillage_The_Village/Assets/Scripts/ScoreAndTime/TimerScript.cs
+++ Pillage_The_Village/Assets/Scripts/ScoreAndTime/TimerScript.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
 
 public class TimerScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{ 
+public int timeLeft = 60;
+public Text countDown;
+public Text GameOver;
+public GameObject MPannel;
+// Start is called before the first frame update
+void Start()
     {
-        
+        StartCoroutine("LoseTime");
+        Time.timeScale = 1;
+        MPannel = GameObject.Find("Main_Menu_Pannel");
+        MPannel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        countDown.text = ("" + timeLeft);
+        if (timeLeft <= 0)
+        {
+            timeLeft = 0;
+            GameOver.text = ("Game Over");
+            MPannel.SetActive(true);
+        }
+    }
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
     }
 }
