7e4884d14521d7b636dc27a529b8fe4fb20da7ee
Will M <willmcb234@gmail.com>
2020-04-08 16:52:36 -0400

    Starting Inheritance

---
+++ Pillage_The_Village/Assets/Scripts/enemyScripts/EnemyParent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyParent : MonoBehaviour
+{
+    public int Health;
+    public int Armor;
+    public string EnemyType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
