881f609923ee3e679a6cb3cfc46ffb837c514e62
Will M <willmcb234@gmail.com>
2020-05-05 00:37:49 -0400

    Minor Stuff changed

--- Pillage_The_Village/Assets/Scripts/PlayerScripts/PlayerAgressivenessTracker.cs
+++ Pillage_The_Village/Assets/Scripts/PlayerScripts/PlayerAgressivenessTracker.cs
@@ -5,16 +5,4 @@ using UnityEngine;
 public class PlayerAgressivenessTracker : MonoBehaviour
 {
     public bool PlayerIsAgressive;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
